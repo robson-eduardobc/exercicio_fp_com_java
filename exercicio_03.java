c0acdea70d823e946a30497e54016891f71d3003
Robson <robsoneduardobc@gmail.com>
2026-04-12 22:32:19 -0300

    Criar variável número inteiro

---
+++ exercicio_03.java
@@ -0,0 +1,11 @@
+public class exercicio_03 {
+    public static void main(String[] args) {
+        // Será criado uma variável número inteiro e atribuído o valor 4
+        int numero = 4;
+
+        // Para exibir o valor na tela
+        System.out.println(numero);
+
+    }
+    
+}
