bee2ead357a797dba67853be69272a9b42a678ce
Robson <robsoneduardobc@gmail.com>
2026-04-12 21:47:39 -0300

    Mudança no print

---
+++ exercicio_01.java
@@ -0,0 +1,6 @@
+public class exercicio_01 {
+    public static void main(String[] args) {
+        // O comando System.out.println exibirá o texto e pulará uma linha
+        System.out.println("Olá mundo!");
+    }
+}
